c5a8476d0da995000aaadd18830decc937d1e751
Yogapratama1109 <yoga110011@gmail.com>
2024-02-16 09:59:34 +0700

    update

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -30,7 +30,7 @@
 </head>
 
 <body class="g-sidenav-show   bg-gray-100">
-    <div class="min-height-300 bg-primary position-absolute w-100"></div>
+    <div class="min-height-300 position-absolute w-100" style="background-color: #5e72e4"></div>
     <aside
         class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 "
         id="sidenav-main">
